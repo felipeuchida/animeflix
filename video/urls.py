3cf5f04f2a2583797bd7433f2ab4aaf5fbf5e818
felipeuchida <felipeuchida1@gmail.com>
2017-06-06 21:52:50 -0300

    before gets realy pretty

--- video/urls.py
+++ video/urls.py
@@ -4,12 +4,6 @@ from . import views
 app_name = 'video'
 
 urlpatterns = [
-	# /video/
-    url(r'^$', views.index, name='index'),
-
-    # /video/71/   71 é o id(exemplo)
-    url(r'^(?P<series_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /video/71/favorite   71 é o id(exemplo)
-    url(r'^(?P<series_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
