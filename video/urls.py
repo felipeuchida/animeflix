33297daf08594114a72a6f731673cab02e00882c
felipeuchida <felipeuchida1@gmail.com>
2017-06-07 04:01:58 -0300

    kbo

--- video/urls.py
+++ video/urls.py
@@ -5,6 +5,7 @@ app_name = 'video'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'series/add/$', views.SeriesCreate.as_view(), name='series-add'),
     url(r'series/(?P<pk>[0-9]+)/$', views.SeriesUpdate.as_view(), name='series-update'),
--- video/views.py
+++ video/views.py
@@ -46,3 +46,12 @@ class UserFormView(View):
 			password = form.cleaned_data['password']
 			user.set_password(password)
 			user.save()
+
+			user = authenticate(username=username, password=password)
+
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return redirect('video:index')
+
+		return render(request, self.template_name, {'form':form})
